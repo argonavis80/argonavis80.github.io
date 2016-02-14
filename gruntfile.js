4c8f5e568d8ae5c4bbfdf3e319501a86174d1ffd
Henrik Alfke <henrik.alfke@live.de>
2016-02-14 01:04:38 +0100

    Added jQuery and bootstrap. Added collapsible and affix navigation.

---
+++ gruntfile.js
@@ -0,0 +1,53 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        copy: {
+            js: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            "bower_components/jquery/dist/jquery.min.js",
+                            "bower_components/bootstrap/dist/js/bootstrap.min.js"
+                        ],
+                        dest: "js",
+                        filter: "isFile"
+                    }
+                ]
+            },
+            css: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            "bower_components/bootstrap/dist/css/bootstrap.min.css",
+                            "bower_components/font-awesome/css/font-awesome.min.css"
+                        ],
+                        dest: "css",
+                        filter: "isFile"
+                    }
+                ]
+            },
+            fonts: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            "bower_components/bootstrap/dist/fonts/*",
+                            "bower_components/font-awesome/fonts/*"
+                        ],
+                        dest: "fonts",
+                        filter: "isFile"
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+
+    grunt.registerTask("default", ["copy"]);
+};
---
+++ js/site.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() {
+    //$(".nav label").click(function () {
+    //    $(this).parents("ul").children("label").siblings().slideUp();
+    //    //$("#navlist").find("label").siblings().slideUp();
+    //    $(this).siblings().slideDown();
+    //});
+
+    $("[data=tree-toggler]").click(function () {
+        $(this).parent().parent().find("[data=tree-toggler]").siblings().slideUp();
+        $(this).siblings().slideDown();
+    });
+});
